685619bf29411214584dd1f94a5b50b741a49270
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-07-21 22:42:33 +0100

    setup whitenoise config

--- website/settings.py
+++ website/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # 3rd party apps
+    "whitenoise",
     "tailwind",
     "storages",
     "theme",
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
