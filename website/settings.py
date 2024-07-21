7ddc80df933226ac283dc78a0ab16ff5a6cc28b1
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-07-21 16:00:06 +0100

    added static and made migrations

--- website/settings.py
+++ website/settings.py
@@ -143,7 +143,7 @@ if not DEBUG:
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # Default primary key field type
